8d8301c92dafb3194f29774839a28117dfe7bb76
Pamela Sanchez <pamela.sann@hotmail.com>
2025-04-23 16:47:41 -0600

    testing formatter, modified format

--- src/test/java/com/example/microservicedemo/MicroservicedemoApplicationTests.java
+++ src/test/java/com/example/microservicedemo/MicroservicedemoApplicationTests.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicroservicedemoApplicationTests {
+class MicroservicedemoApplicationTests 
+{
 
     @Test
     void contextLoads() {}
